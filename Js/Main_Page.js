3ae8d6fc35164e5e3a0955ff1d5bbff39c190269
Roshan4555 <surukutlaroshan@gmail.com>
2020-06-27 17:47:30 +0530

    UI uppdated

---
+++ Js/Main_Page.js
@@ -0,0 +1,20 @@
+ function myFunction(){
+              document.getElementById("myDropdown").classList.toggle("show"); 
+              }
+              window.onclick = function(event) {
+              if (!event.target.matches('.dropbtn')) {
+              var dropdowns = document.getElementsByClassName("dropdown-content");
+              var i;
+              for (i = 0; i < dropdowns.length; i++) {
+              var openDropdown = dropdowns[i];
+              if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+                 }
+                }
+               }
+              }
+
+                function Scrollup(){
+        document.body.scrollTop=0;
+        document.documentElement.scrollTop=0;
+      }  
